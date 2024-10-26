1963ead006f04a9ae2b676d9765393600d984c9a
sx.king <mdrs.plug@gmail.com>
2024-10-26 17:23:47 -0300

    Primeiro Commit

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,7 @@
+numeroum = int(input('Digite um número inteiro: '))
+numerodois = int(input('Digite outro número inteiro: '))
+soma = numeroum + numerodois
+subtracao = numeroum - numerodois
+
+print(f'A soma de {numeroum} com {numerodois} resulta em {soma}!')
+print(f'A subtração de {numeroum} com {numerodois} resulta em {subtracao}!')
